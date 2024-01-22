74917a2a0968695f36c2e12e5a0ecff989d1801a
jakkapatB <96049107+JakkapatB@users.noreply.github.com>
2024-01-22 22:27:49 +0700

    add app bar

--- lib/pages/workzone.dart
+++ lib/pages/workzone.dart
@@ -10,28 +10,50 @@ class Workzone extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Centered Text Example',
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: HexColor('#245798'),
-        ),
-        body: Center(
-          child: Container(
-            color: HexColor('#245798'),
-            child: const Center(
-              child: Text(
-                'Work zone',
-                style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
-              ),
+          appBar: AppBar(
+            backgroundColor: HexColor('#245798'),
+            title: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Work Zone',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/cat.jpg'),
+                ),
+              ],
             ),
           ),
-        ),
-      ),
+          body: Container( //This is the largest box that is the body of the app.
+            color: HexColor('#245798'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                //This is the section that follows the app bar and is used to add sections next to the app bar.
+                Container(
+                  height: 116,
+                  decoration: BoxDecoration(
+                    color: HexColor('#245798'),
+                    borderRadius: const BorderRadius.only(
+                        bottomRight: Radius.circular(24.0),
+                        bottomLeft: Radius.circular(24.0)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: HexColor('#333333').withOpacity(0.2),
+                        offset: const Offset(0, 4),
+                        blurRadius: 13.9,
+                        spreadRadius: -3,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
